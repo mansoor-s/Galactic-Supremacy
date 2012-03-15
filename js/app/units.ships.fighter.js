7eaed8b467af9fc622a7cd36ca88444bebc19ac7
ivankuzev <ivankuzev@hotmail.com>
2012-03-15 19:16:35 +0200

    basic preloader added

--- js/app/units.ships.fighter.js
+++ js/app/units.ships.fighter.js
@@ -1,8 +1,9 @@
 (function() {
     "use strict";
     var Fighter = App.Units.Ships.Fighter = function(){
-        this.launched = false;
+         Fighter.inheritsFrom(App.Units.Ships.Base);
+         this.launched = false;
     }  
-    Fighter.inheritsFrom(App.Units.Ships.Base);
+  
   
 })();
